23e6519ec380e3b38464c6dd1e69d3e994a6b84c
alsher00alsher-alt <alsher00alsher@gmail.com>
2026-09-03 21:44:30 +0300

    Integrate index.html frontend directly into server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 app.get('/', (req, res) => {
-    res.json({ status: 'success', message: 'APK Builder Server is running' });
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/build', (req, res) => {
